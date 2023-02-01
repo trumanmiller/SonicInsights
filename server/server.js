97c58f16936285ecc761472155cfc9349d5e007a
Truman Miller <wesleythemiller@gmail.com>
2023-01-31 20:05:10 -0600

    day 1 finish

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const path = require('path');
 
+const spotifyRouter = require(path.join(__dirname, './routes/spotify'));
+
 const app = express();
 const PORT = 3000;
 
-app.use(express.json())
-// const router
+app.use(express.json());
 
+// app.use('/spotify', spotifyRoputer);
 
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
+})
 
 app.listen(PORT, () => {
   console.log(`LISTEN ON PORT ${PORT}`);
-})
+});
--- webpack.config.js
+++ webpack.config.js
@@ -31,4 +31,33 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    // enable HMR on the devServer
+    hot: true,
+    // fallback to root for other urls
+    // historyApiFallback: true,
+
+    static: {
+      // match the output path
+      directory: './index.html',
+      // match the output 'publicPath'
+      publicPath: '/',
+    },
+
+    // headers: { 'Access-Control-Allow-Origin': '*' },
+    proxy: {
+      '/api/**': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      },
+    },
+  },
+  // proxy: {
+  //   '/api/**': {
+  //     target: 'http://localhost:3000/',
+  //     secure: false,
+  //   },
+  // },
 };
